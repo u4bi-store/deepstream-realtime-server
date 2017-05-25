43bd0c189cf7e74c844dc9068dc44776bcba2085
u4bi <u4bi@u4bi.com>
2017-05-25 23:44:16 +0900

    angular cli import deepstream
    - console test

--- angular-deep/src/app/app.component.ts
+++ angular-deep/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var deepstream : any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(){
+    console.log(deepstream);
+  }
 }
